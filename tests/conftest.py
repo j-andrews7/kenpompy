2c7431cab9eca1f4498e1de741ad5b6fb3be6d83
Jared Andrews <jared.andrews07@gmail.com>
2019-12-22 23:07:55 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,6 @@ from kenpompy.utils import login
 @pytest.fixture(scope="session")
 def browser():
     bowser = login(os.environ["EMAIL"], os.environ["PASSWORD"])
+    print(os.environ["EMAIL"])
+    print(os.environ["PASSWORD"])
     return bowser
