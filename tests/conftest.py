c6ec8dc94f8178fd58b17971a0604f6b6aecf94a
Jared Andrews <jared.andrews07@gmail.com>
2019-12-22 22:57:28 -0600

    More test setup changes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 import pytest
 from kenpompy.utils import login
 
-def pytest_generate_tests(metafunc):
-    if "browser" in metafunc.fixturenames:
-        browser = login(os.environ["EMAIL"], os.environ["PASSWORD"])
-        metafunc.parametrize("browser", browser)
+@pytest.fixture(scope="session")
+def get_browser():
+    browser = login(os.environ["EMAIL"], os.environ["PASSWORD"])
+    return browser
