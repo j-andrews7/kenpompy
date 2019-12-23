d88f258bed74a2bfde74e05fa51e4442f9573169
Jared Andrews <jared.andrews07@gmail.com>
2019-12-22 23:01:52 -0600

    Typo.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import pytest
 from kenpompy.utils import login
 
 @pytest.fixture(scope="session")
-def get_browser():
-    browser = login(os.environ["EMAIL"], os.environ["PASSWORD"])
-    return browser
+def browser():
+    bowser = login(os.environ["EMAIL"], os.environ["PASSWORD"])
+    return bowser
