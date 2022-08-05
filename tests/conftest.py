a677b1eaaafc5f95ea312047b082d988129daa6b
Sean F Quinn <sean@esqew.com>
2022-08-04 22:18:02 -0400

    added logic to detect when login fails due to incorrect credentials

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,9 @@ from kenpompy.utils import login
 
 @pytest.fixture(scope="session")
 def browser():
-	bowser = login(os.environ["EMAIL"], os.environ["PASSWORD"])
-	return bowser
+	try:
+		browser = login(os.environ["EMAIL"], os.environ["PASSWORD"])
+	except Exception as e:
+		pytest.exit(e)
+
+	return browser
