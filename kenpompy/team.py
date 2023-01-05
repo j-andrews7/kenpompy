f156a1b0383b26c0d92704e85ccb8530628698ab
Nick Ostendorf <nickostendorf@gmail.com>
2023-01-05 13:39:58 -0500

    fix spacing and comments

--- kenpompy/team.py
+++ kenpompy/team.py
@@ -36,7 +36,7 @@ def get_valid_teams(browser, season=None):
 	team_df = pd.read_html(str(table))
 	# Get only the team column.
 	team_df = team_df[0].iloc[:, 1]
- 	# Remove NCAA tourny seeds for previous seasons.
+	 # Remove NCAA tourny seeds for previous seasons.
 	team_df = team_df.str.replace(r'\d+', '', regex=True)
 	team_df = team_df.str.rstrip()
 	team_df = team_df.dropna()
@@ -45,8 +45,6 @@ def get_valid_teams(browser, season=None):
 	team_list = [team for team in team_df if team != "Team"]
 
 	return team_list
-
-
 
 
 def get_schedule(browser, team=None, season=None):
@@ -83,10 +81,10 @@ def get_schedule(browser, team=None, season=None):
 	else:
 		season = int(currentYear)
 
-	if team==None or team not in get_valid_teams(browser, season):
-			raise ValueError(
-				'the team does not exist in kenpom in the given year.  Check that the spelling matches (https://kenpom.com) exactly.')
-	
+	if team == None or team not in get_valid_teams(browser, season):
+		raise ValueError(
+			'the team does not exist in kenpom in the given year.  Check that the spelling matches (https://kenpom.com) exactly.')
+
 	# Sanitize team name
 	team = team.replace(" ", "+")
 	team = team.replace("&", "%26")
@@ -101,86 +99,87 @@ def get_schedule(browser, team=None, season=None):
 	# Dataframe Tidying
 	schedule_df = schedule_df[0]
 	schedule_df.columns = ['Date', 'Team Rank', 'Opponent Rank', 'Opponent Name', 'Result', 'Possession Number',
-					  'A', 'Location', 'Record', 'Conference', 'B']
-	schedule_df = schedule_df.drop(columns = ['A', 'B'])
+						'A', 'Location', 'Record', 'Conference', 'B']
+	schedule_df = schedule_df.drop(columns=['A', 'B'])
 	schedule_df = schedule_df.fillna('')
 
 	return schedule_df
 
+
 def get_stats(browser, team=None, season=None):
-    """
-    Scrapes a team's stats from (https://kenpom.com/team.php) into a dataframe.
+	"""
+	Scrapes a team's stats from (https://kenpom.com/team.php) into a dataframe.
 
-    Args:
-        browser (mechanicalsoul StatefulBrowser): Authenticated browser with full access to kenpom.com generated
-            by the `login` function
-        team: Used to determine which team to scrape for schedule.
-        season (str, optional): Used to define different seasons. 2002 is the earliest available season.
+	Args:
+		browser (mechanicalsoul StatefulBrowser): Authenticated browser with full access to kenpom.com generated
+			by the `login` function
+		team: Used to determine which team to scrape for stats.
+		season (str, optional): Used to define different seasons. 2002 is the earliest available season.
 
-    Returns:
-        stats_df (pandas dataframe): Dataframe containing a team's schedule for the given season.
+	Returns:
+		stats_df (pandas dataframe): Dataframe containing a team's stats for the given season.
 
-    Raises:
-        ValueError if `season` is less than 2002.
-        ValueError if `season` is greater than the current year.
-        ValueError if `team` is not in the valid team list.
-    """
+	Raises:
+		ValueError if `season` is less than 2002.
+		ValueError if `season` is greater than the current year.
+		ValueError if `team` is not in the valid team list.
+	"""
 
-    url = 'https://kenpom.com/team.php'
+	url = 'https://kenpom.com/team.php'
 
-    date = datetime.date.today()
-    currentYear = date.strftime("%Y")
+	date = datetime.date.today()
+	currentYear = date.strftime("%Y")
 
-    if season:
-        if int(season) < 2002:
-            raise ValueError(
-                'season cannot be less than 2002, as data only goes back that far.')
-        if int(season) > int(currentYear):
-            raise ValueError(
-                'season cannot be greater than the current year.')
-    else:
-        season = int(currentYear)
+	if season:
+		if int(season) < 2002:
+			raise ValueError(
+				'season cannot be less than 2002, as data only goes back that far.')
+		if int(season) > int(currentYear):
+			raise ValueError(
+				'season cannot be greater than the current year.')
+	else:
+		season = int(currentYear)
 
-    if team == None or team not in get_valid_teams(browser, season):
-            raise ValueError(
-                'the team does not exist in kenpom in the given year.  Check that the spelling matches (https://kenpom.com) exactly.')
+	if team == None or team not in get_valid_teams(browser, season):
+		raise ValueError(
+			'the team does not exist in kenpom in the given year.  Check that the spelling matches (https://kenpom.com) exactly.')
 
-    # Sanitize team name
-    team = team.replace(" ", "+")
-    team = team.replace("&", "%26")
-    url = url + "?team=" + str(team)
-    url = url + "&y=" + str(season)
+	# Sanitize team name
+	team = team.replace(" ", "+")
+	team = team.replace("&", "%26")
+	url = url + "?team=" + str(team)
+	url = url + "&y=" + str(season)
 
-    browser.open(url)
-    team_page = browser.get_current_page()
+	browser.open(url)
+	team_page = browser.get_current_page()
 
-    # Save page to temporarily
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.html') as file:
-        file.write(team_page.encode())
+	# Save page to temporarily
+	with tempfile.NamedTemporaryFile(delete=False, suffix='.html') as file:
+		file.write(team_page.encode())
 
-    # Open requests_html session with saved paged
-    """
+	# Open saved paged with requests_html session
+	"""
 	While using requests_html and request_file
 	we are able to render the javascript on teams page
 	that mechanicalSoup is unable to do.
 	"""
-    sess = HTMLSession()
-    sess.mount('file://', FileAdapter())
-    r = sess.get(f'file:///{file.name}')
-    r.html.render()
+	sess = HTMLSession()
+	sess.mount('file://', FileAdapter())
+	r = sess.get(f'file:///{file.name}')
+	r.html.render()
 
 	# Create soup object from rendered page
-    soup = BeautifulSoup(r.html.html, 'html.parser')
+	soup = BeautifulSoup(r.html.html, 'html.parser')
 
 	# Retrieve stats table and parse into DataFrame
-    statsTable = soup.findAll('table')[0]
-    stats_df = pd.read_html(str(statsTable))[0]
+	statsTable = soup.findAll('table')[0]
+	stats_df = pd.read_html(str(statsTable))[0]
 
-    # DataFrame cleaning, drop repeated column names
-    stats_df.drop([3,8,15,18,22,26], inplace=True)
+	# DataFrame cleaning, drop repeated column names
+	stats_df.drop([3, 8, 15, 18, 22, 26], inplace=True)
 
-    # Remove stat rankings
-    stats_df['Offense'] = stats_df['Offense'].str.split().str[0]
-    stats_df['Defense'] = stats_df['Defense'].str.split().str[0]
+	# Remove stat rankings
+	stats_df['Offense'] = stats_df['Offense'].str.split().str[0]
+	stats_df['Defense'] = stats_df['Defense'].str.split().str[0]
 
-    return stats_df
+	return stats_df
