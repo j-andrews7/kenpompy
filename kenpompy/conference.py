091383f017feec6c767fb982d9f59a77895a1b9a
WakeUpWaffles <haiflichl@gmail.com>
2023-03-19 13:40:14 -0400

    Updated spacing between/in functions aggregate stats can be pulled for all confs at once

--- kenpompy/conference.py
+++ kenpompy/conference.py
@@ -1,5 +1,5 @@
 """
-This module contains functions for scraping the team page kenpom.com tables into
+This module contains functions for scraping the confernce page kenpom.com tables into
 pandas dataframes
 """
 
@@ -12,7 +12,7 @@ import datetime
 
 def get_valid_conferences(browser, season=None):
 	"""
-	Scrapes the conferences (https://kenpom.com) into a list.
+	Scrapes the conferences (https://kenpom.com/conf.php) into a list.
 
 	Args:
 		browser (mechanicalsoul StatefulBrowser): Authenticated browser with full access to kenpom.com generated
@@ -34,48 +34,61 @@ def get_valid_conferences(browser, season=None):
 	conf_list = []
 	for link in links:
 		conf_list.append(link['href'].split('=')[-1])
-	
+	conf_list.sort()
 	return conf_list
 
-def get_aggregate_stats(browser, conf, season=None):
+
+def get_aggregate_stats(browser, conf=None, season=None):
 	"""
-	Scrapes a given conference's stats (https://kenpom.com) into a dataframe.
+	Scrapes a given conference's stats (https://kenpom.com/conf.php or https://kenpom.com/confstats.php) into a dataframe.
 
 	Args:
 		browser (mechanicalsoul StatefulBrowser): Authenticated browser with full access to kenpom.com generated
 			by the `login` function
-		conf (str): conference abbreviation (ie B10, P12)
+		conf (str: optional): conference abbreviation (ie B10, P12). If None, it will grab the table from https://kenpom.com/confstats.php instead of https://kenpom.com/conf.php
 		season (str, optional): Used to define different seasons. 2002 is the earliest available season.
 
 	Returns:
 		conference_df (dataframe): Dataframe containing aggregate stats of the conference for the given season on kenpom.com.
 	"""
-
-	url = "https://kenpom.com/conf.php"
-	url = url + f'?c={conf}'
-	if(season):
-		url = url + '&y=' + str(season)
-	browser.open(url)
-	confs = browser.get_current_page()
-	#get first table
-	table = confs.find_all('table')[-3]
-	conf_df = pd.read_html(str(table))[0]
-	#get second table
-	table = confs.find_all('table')[-2]
-	conf2_df = pd.read_html(str(table))[0]
-	conf2_df['Value'] = conf2_df['Value'].str.replace('%', '').astype(float)
-	conf_df = pd.concat([conf_df, conf2_df])
-	#clean table
-	conf_df = conf_df.set_index('Stat')
-	conf_df = conf_df.drop('Unnamed: 1', axis=1)
-	conf_df.columns = ['Value', 'Rank']
-	conf_df.index = conf_df.index.str.split(' (', regex=False).str[0]
-	print(conf_df)
-	return conf_df
+	if(conf):
+		url = "https://kenpom.com/conf.php"
+		url = url + f'?c={conf}'
+		if(season):
+			url = url + '&y=' + str(season)
+		browser.open(url)
+		confs = browser.get_current_page()
+		#get first table
+		table = confs.find_all('table')[-3]
+		conf_df = pd.read_html(str(table))[0]
+		#get second table
+		table = confs.find_all('table')[-2]
+		conf2_df = pd.read_html(str(table))[0]
+		conf2_df['Value'] = conf2_df['Value'].str.replace('%', '').astype(float)
+		conf_df = pd.concat([conf_df, conf2_df])
+		#clean table
+		conf_df = conf_df.set_index('Stat')
+		conf_df = conf_df.drop('Unnamed: 1', axis=1)
+		conf_df.columns = ['Value', 'Rank']
+		conf_df.index = conf_df.index.str.split(' (', regex=False).str[0]
+		return conf_df
+	else:
+		url = "https://kenpom.com/confstats.php"
+		if(season):
+			url = url + '?y=' + str(season)
+		browser.open(url)
+		confs = browser.get_current_page()
+		#get table
+		table = confs.find_all('table')[0]
+		conf_df = pd.read_html(str(table))[0]
+		# Clean table
+		conf_df = conf_df.set_index('Conf')
+		conf_df.columns = [stat[:-1] + 'Rank' if '.1' in stat else stat for stat in conf_df.columns]
+		return conf_df
 
 def get_standings(browser, conf, season=None):
 	"""
-	Scrapes a given conference's standing stats (https://kenpom.com) into a dataframe.
+	Scrapes a given conference's standing stats (https://kenpom.com/conf.php) into a dataframe.
 
 	Args:
 		browser (mechanicalsoul StatefulBrowser): Authenticated browser with full access to kenpom.com generated
@@ -102,12 +115,12 @@ def get_standings(browser, conf, season=None):
 	# Rename Rank headers
 	conf_df.columns = [stat[:-1] + 'Rank' if '.1' in stat else stat for stat in conf_df.columns]
 
-	print(conf_df)
 	return conf_df
+
 
 def get_offense(browser, conf, season=None):
 	"""
-	Scrapes a given conference's offense only stats (https://kenpom.com) into a dataframe.
+	Scrapes a given conference's offense only stats (https://kenpom.com/conf.php) into a dataframe.
 
 	Args:
 		browser (mechanicalsoul StatefulBrowser): Authenticated browser with full access to kenpom.com generated
@@ -131,8 +144,8 @@ def get_offense(browser, conf, season=None):
 	# Rename Rank headers
 	conf_df.columns = [stat[:-1] + 'Rank' if '.1' in stat else stat for stat in conf_df.columns]
 
-	print(conf_df)
 	return conf_df
+
 
 def get_defense(browser, conf, season=None):
 	"""
@@ -160,5 +173,4 @@ def get_defense(browser, conf, season=None):
 	# Rename Rank headers
 	conf_df.columns = [stat[:-1] + 'Rank' if '.1' in stat else stat for stat in conf_df.columns]
 
-	print(conf_df)
 	return conf_df
--- tests/test_conference.py
+++ tests/test_conference.py
@@ -12,6 +12,8 @@ def test_get_valid_conferences(browser):
 	valid_2021_confs = ['B12', 'P12', 'Pat']
 	for team in valid_2021_confs:
 		assert team in confs_2021
+	assert confs_2021[0] == 'A10'
+	assert confs_2021[-1] == 'WCC'
 
 	confs_2003 = kpconf.get_valid_conferences(browser, season = '2003')
 	assert len(confs_2003) == 32
@@ -19,6 +21,9 @@ def test_get_valid_conferences(browser):
 	valid_2003_confs = ['MEAC', 'CAA']
 	for team in valid_2003_confs:
 		assert team in confs_2003
+	assert confs_2003[0] == 'A10'
+	assert confs_2003[-1] == 'ind'
+
 
 def test_get_aggregate_stats(browser):
 	expectedTempo = 67.9
@@ -26,16 +31,12 @@ def test_get_aggregate_stats(browser):
 	expectedHomeWinPercent = 60.3
 	expectedHomeWinPercentRank = 8
 
-
 	confs_2021 = kpconf.get_aggregate_stats(browser, 'B10', season = '2021')
 	assert confs_2021.loc['Tempo', 'Value'] == expectedTempo
 	assert confs_2021.loc['Tempo', 'Rank'] == expectedTempoRank
 	assert confs_2021.loc['Home win%', 'Value'] == expectedHomeWinPercent
 	assert confs_2021.loc['Home win%', 'Rank'] == expectedHomeWinPercentRank
 	
-
-
-
 	expectedTempo = 65.5
 	expectedTempoRank = 27
 	expectedHomeWinPercent = 71.6
@@ -47,6 +48,21 @@ def test_get_aggregate_stats(browser):
 	assert confs_2003.loc['Home win%', 'Value'] == expectedHomeWinPercent
 	assert confs_2003.loc['Home win%', 'Rank'] == expectedHomeWinPercentRank
 
+	expectedTempo = 67
+	expectedTempoRank = 28
+
+	confs_2021 = kpconf.get_aggregate_stats(browser, season = '2021')
+	assert confs_2021.loc['AE', 'Tempo'] == expectedTempo
+	assert confs_2021.loc['AE', 'Tempo.Rank'] == expectedTempoRank
+
+	expectedTempo = 67.2
+	expectedTempoRank = 20
+
+	confs_2003 = kpconf.get_aggregate_stats(browser, season = '2003')
+	assert confs_2003.loc['AE', 'Tempo'] == expectedTempo
+	assert confs_2003.loc['AE', 'Tempo.Rank'] == expectedTempoRank
+
+
 def test_get_standings(browser):
 	expectedTeam1 = 'Michigan'
 	expectedTeam1AdjO = 117.6
@@ -57,7 +73,6 @@ def test_get_standings(browser):
 	assert confs_2021.iloc[0, :]['AdjO'] == expectedTeam1AdjO
 	assert confs_2021.iloc[0, :]['AdjO.Rank'] == expectedTeam1AdjORank
 
-	
 	expectedTeam1 = 'Wisconsin'
 	expectedTeam1AdjO = 114.4
 	expectedTeam1AdjORank = 16	
@@ -66,6 +81,7 @@ def test_get_standings(browser):
 	assert confs_2003.iloc[0, :]['Team'] == expectedTeam1
 	assert confs_2003.iloc[0, :]['AdjO'] == expectedTeam1AdjO
 	assert confs_2003.iloc[0, :]['AdjO.Rank'] == expectedTeam1AdjORank
+
 
 def test_get_offense(browser):
 	expectedTeam1 = 'Iowa'
@@ -77,7 +93,6 @@ def test_get_offense(browser):
 	assert confs_2021.iloc[0, :]['Tempo'] == expectedTeam1Tempo
 	assert confs_2021.iloc[0, :]['Tempo.Rank'] == expectedTeam1TempoRank
 
-	
 	expectedTeam1 = 'Wisconsin'
 	expectedTeam1Tempo = 61.5
 	expectedTeam1TempoRank = 11	
@@ -86,6 +101,7 @@ def test_get_offense(browser):
 	assert confs_2003.iloc[0, :]['Team'] == expectedTeam1
 	assert confs_2003.iloc[0, :]['Tempo'] == expectedTeam1Tempo
 	assert confs_2003.iloc[0, :]['Tempo.Rank'] == expectedTeam1TempoRank
+
 
 def test_get_defense(browser):
 	expectedTeam1 = 'Michigan'
@@ -96,7 +112,6 @@ def test_get_defense(browser):
 	assert confs_2021.iloc[0, :]['Team'] == expectedTeam1
 	assert confs_2021.iloc[0, :]['Stl%'] == expectedTeam1Stl
 	assert confs_2021.iloc[0, :]['Stl%.Rank'] == expectedTeam1StlRank
-
 
 	expectedTeam1 = 'Illinois'
 	expectedTeam1Stl = 9.7
